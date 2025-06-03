a997367b18aa0c2523be2e6f9861e7a9e04c5968
Tingan <syeak123@gmail.com>
2025-06-03 16:36:23 +0800

    feat:add input button

--- my-react-training/src/App.jsx
+++ my-react-training/src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 
 function App(){
   const [showText,setshowText]=useState(true)
   const [inputText,setinputText]=useState('')
+  const [messages,setMessages]=useState([])
   const [step,setStep]=useState(0)
+
+  const sendMessages=()=>{
+    if (inputText.trim()==="")
+      return;
+    else console(`${inputText}`)
+
+  }
   return(<>
     <div>
       <button onClick={()=>setshowText(!showText)}>
@@ -12,14 +20,15 @@ function App(){
       {showText?<p>第一個訊息</p>:<p>我切換了</p>}
     </div>
     <div>
-      <input
-      type="text"
-      placeholder='請輸入訊息'
-      value={inputText}
-      onChange={(e)=>setinputText(e.target.value)}
-      />
-      
+<input type="text"
+       placeholder='請輸入訊息'
+       value={inputText}
+       onChange={(e)=>setinputText(e.target.value)} />
+<button onClick={()=>sendMessages()}>送出訊息</button>
+    
       <p>你輸入的是:{inputText}</p>
+      <ul></ul>
+     {inputText && <button onClick={()=>setinputText('')}>清除字幕</button>}
     </div>
     <div>
       <p>第{step}步</p>
