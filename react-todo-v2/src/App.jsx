09aa5904b4c8cf5fbe0e135373c9037f85b0599f
Tingan <syeak123@gmail.com>
2025-07-23 13:52:07 +0800

    feat:add new task to the list via input

--- react-todo-v2/src/App.jsx
+++ react-todo-v2/src/App.jsx
@@ -1,18 +1,36 @@
 import { useState } from "react";
 
 function App() {
-  const [todo, seTodo] = useState([
+  const [task, setTask] = useState([
     { id: 1, text: "練習React", done: false },
     { id: 2, text: "吃午餐", done: false },
   ]);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTask=()=>{
+  if (inputValue.trim() === "") return;
+
+  const newTask = {
+    id: Date.now(),
+    text: inputValue,
+    done: false,
+  };
+  setTask([...task,newTask]);
+  setInputValue('');}
   return (
     <div style={{ padding: "2rem" }}>
       <h1>我的待辦事項</h1>
-      <input type="text" placeholder="輸入任務" />
-      <button>新增</button>
+      <input
+        type="text"
+        placeholder="輸入任務"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddTask}>新增</button>
 
       <ul>
-        {todo.map((task) => (
+        {task.map((task) => (
           <li key={task.id}>
             {task.done ? "✅" : "⬜"} {task.text}
           </li>
