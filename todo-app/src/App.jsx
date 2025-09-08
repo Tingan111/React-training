aa7596c6549b6433c757c4e2c6dff4e24f71ea8d
Tingan <syeak123@gmail.com>
2025-09-08 18:43:35 +0800

    feat:add create and remove

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,27 +1,40 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-function App(){
-  const [input,setInput]=useState('');
-  const [list,setList]=useState([]);
+function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const add = () => {
+    if (input.trim() !== "") {
+      setTodos([...todos, input]);
+      
+      setInput("");
+    }
+  };
 
-  const handleAdd=()=>{
-    if(input.trim()==='')return;
-    setList([...list,input]);
-    setInput('');
-    console.log(list);
-    
-  }
-  return(<>
-  <div>
-    <input value={input}
-    onChange={(e)=>setInput(e.target.value)}
-    />
-    <button onClick={handleAdd} >新增</button>
-  </div>
-  <ul>
-    {list.map((item,i)=>(<li key={i}>{item}</li>))}
-  </ul>
-  </>
-)}
+const remove=(removeIndex)=>{
+setTodos(todos.filter((todo,index)=>index!==removeIndex))
+;
 
-export default App
+}
+const update=(editIndex)=>setTodos(todos.filter((todo,index)=>index!==editIndex))
+
+  return (
+    <div>
+      <h1>待辦事項</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />{" "}
+      <button onClick={add}>新增</button>
+      {todos.map((todo, index) => (
+        <div key={index}>
+          {todo}
+          <button onClick={()=>remove(index)}>刪除</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
